e5461263c3fa2a2fc3611d3ff4f852f7b3598630
Marvin <marvincudjoe@outlook.com>
2021-11-18 15:18:46 +0000

    Update to Tests (#7)
    - Add additional dependencies for creating tests.
    - Add test to check debug entries in the logs
    - minor code restructure
    - configurations.all to prevent SLF4J "multiple bindings" error.

--- src/test/java/uk/gov/hmcts/fortifyclient/FortifyClientTest.java
+++ src/test/java/uk/gov/hmcts/fortifyclient/FortifyClientTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.fortifyclient;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -9,11 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FortifyClientTest {
+    FortifyClientConfig config;
+    FortifyClient fortifyClient;
+
+    @BeforeEach
+    void setup() throws Exception {
+        config = FortifyClientConfig.getNewDefaultInstance();
+        fortifyClient = new FortifyClient(config);
+    }
 
     @Test
     void should_fail_scan_with_wrong_credentials() throws Exception {
-        FortifyClientConfig config = FortifyClientConfig.getNewDefaultInstance();
-        FortifyClient fortifyClient = new FortifyClient(config);
         Exception e = Assertions.assertThrows(FortifyClientException.class,
                 fortifyClient::requestScanAndGetResults);
         assertTrue(e.getMessage().contains("code=401, message=Unauthorized"));
@@ -21,8 +28,6 @@ class FortifyClientTest {
 
     @Test
     void should_find_root_folder() throws Exception {
-        FortifyClientConfig config = FortifyClientConfig.getNewDefaultInstance();
-        FortifyClient fortifyClient = new FortifyClient(config);
         File curDir = new File(".").getAbsoluteFile().getCanonicalFile();
         File root = fortifyClient.findRootDirectory(curDir, 0);
         assertNotNull(root);
@@ -31,10 +36,15 @@ class FortifyClientTest {
 
     @Test
     void createFortifyExportDirectory() throws Exception {
-        FortifyClientConfig config = FortifyClientConfig.getNewDefaultInstance();
-        FortifyClient fortifyClient = new FortifyClient(config);
         File file = fortifyClient.getFortifyExportDirectory();
         assertTrue(file.exists());
         assertTrue(file.isDirectory());
+    }
+
+    @Test
+    void checkFortifyPath() {
+        config.getPortalUrl();
+        String g = config.getValueFor(FortifyClientEnvironment.FORTIFY_PORTAL_URL, "fortify.portal.url");
+        assertTrue(g.contains("fortify.com"));
     }
 }

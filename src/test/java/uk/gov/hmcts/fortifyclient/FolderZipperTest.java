0c2d19849fc52544777853629fb2a6d9f48ce33a
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2022-10-19 11:38:20 +0100

    Use logback over uk.org.lidalia

--- src/test/java/uk/gov/hmcts/fortifyclient/FolderZipperTest.java
+++ src/test/java/uk/gov/hmcts/fortifyclient/FolderZipperTest.java
@@ -1,24 +1,26 @@
 package uk.gov.hmcts.fortifyclient;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.List;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-import uk.org.lidalia.slf4jtest.LoggingEvent;
-import uk.org.lidalia.slf4jtest.TestLogger;
-import uk.org.lidalia.slf4jtest.TestLoggerFactory;
-
-import java.io.File;
-import java.util.Objects;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static uk.org.lidalia.slf4jext.Level.DEBUG;
+import org.slf4j.LoggerFactory;
 
 class FolderZipperTest {
-    TestLogger logger = TestLoggerFactory.getTestLogger(FolderZipper.class);
+
+    Logger logger = (Logger) LoggerFactory.getLogger(FolderZipper.class);
+    ListAppender<ILoggingEvent> listAppender;
     FortifyClientConfig config;
     FortifyClient fortifyClient;
     FolderZipper zipper;
@@ -37,33 +39,36 @@ class FolderZipperTest {
 
     @Test
     void should_log_target_file_as_debug() throws Exception {
+        setupLogging();
         zipper.zip(new File("."), fortifyClient.getFortifyExportDirectory(), config.getExcludePatterns());
-        for (LoggingEvent logLevel : logger.getLoggingEvents()) {
-            if (Objects.equals(logLevel.getMessage(), "File targeted : {}"))
-                MatcherAssert.assertThat(logLevel.getLevel(), is(DEBUG));
-            if (Objects.equals(logLevel.getMessage(), "File zipped : {}"))
-                MatcherAssert.assertThat(logLevel.getLevel(), is(DEBUG));
-            if (Objects.equals(logLevel.getMessage(), "File excluded : {}"))
-                MatcherAssert.assertThat(logLevel.getLevel(), is(DEBUG));
-        }
+        List<ILoggingEvent> logsList = listAppender.list;
+        MatcherAssert.assertThat(logsList.get(0).getLevel(), is(Level.INFO));
+        logsList.forEach(loggingEvent -> {
+            if (loggingEvent.getMessage().startsWith("File targeted : {}")
+                    || loggingEvent.getMessage().startsWith("File zipped : {}")
+                    || loggingEvent.getMessage().startsWith("File excluded : {}")) {
+                assertSame(Level.DEBUG, loggingEvent.getLevel());
+            }
+        });
     }
 
     @Test
-    void invalid_source_folder() throws Exception {
+    void invalid_source_folder() {
         IllegalArgumentException err = Assertions.assertThrows(IllegalArgumentException.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                zipper.zip
-                        (new File("/unknown_path/"), fortifyClient.getFortifyExportDirectory(), config.getExcludePatterns());
+                zipper.zip(new File("/unknown_path/"),
+                            fortifyClient.getFortifyExportDirectory(), config.getExcludePatterns());
             }
         });
         assertTrue(err.getMessage().contains("Please provide a folder. Source : "));
     }
 
-    @AfterEach
-    public void clearLoggers() {
-        TestLoggerFactory.clear();
-        logger.clear();
+    private void setupLogging() {
+        logger.detachAndStopAllAppenders();
+        listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
     }
 
 }
